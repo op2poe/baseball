7aa164ab21feaa9c0be7d651bf5299a6103ae4db
Optwo Poe <op2poe@gmail.com>
2012-04-30 21:33:59 -0700

    Added package op2poe.baseball.data.batting, with BattingStats trait.

---
+++ scala/data-structs/src/main/scala/op2poe/baseball/data/batting/BattingStats.scala
@@ -0,0 +1,56 @@
+package op2poe.baseball.data.batting
+
+trait BattingStats {
+
+  def plateAppearances: Int
+  
+  def singles: Int
+  
+  def doubles: Int
+  
+  def triples: Int
+  
+  def homeruns: Int
+  
+  def strikeouts: Int
+  
+  def walks: Int
+  
+  def sacrificeHits: Int
+  
+  def sacrificeFlies: Int
+  
+  def hitByPitch: Int
+  
+  def runs: Int
+  
+  def runsBattedIn: Int
+  
+  final def hits = singles + doubles + triples + homeruns
+  
+  final def totalBases = singles + 2 * doubles + 3 * triples + 4 * homeruns
+  
+  // We do not include Times Reached on Defensive (e.g. Catcher's) Interference. 
+  final def atBats = plateAppearances - walks - sacrificeHits - sacrificeFlies - hitByPitch
+  
+  final def battingAverage = {
+    val ab = atBats
+    if (ab > 0) (1.0 * hits) / ab
+    else 0.0
+  }
+  
+  final def sluggingAverage = {
+    val ab = atBats
+    if (ab > 0) (1.0 * totalBases) / ab
+    else 0.0
+  }
+  
+  final def onBaseAverage = {
+    val denominator = atBats + walks + hitByPitch + sacrificeFlies
+    if (denominator > 0) (1.0 * (hits + walks + hitByPitch)) / denominator
+    else 0.0
+  }
+  
+  final def ops = onBaseAverage + sluggingAverage
+  
+}
