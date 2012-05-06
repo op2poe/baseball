aa68e680c5ffa7ba7feceabccd5db533bfb29e74
Optwo Poe <op2poe@gmail.com>
2012-05-06 07:42:43 -0700

    Added package data.game, with class Die.

---
+++ scala/data-structs/src/main/scala/op2poe/baseball/data/game/Die.scala
@@ -0,0 +1,37 @@
+package op2poe.baseball.data.game
+
+import scala.collection.immutable.TreeMap
+import scala.collection.immutable.SortedMap
+import scala.collection.mutable;
+import scala.util.Random
+
+final class Die[A](val sides: SortedMap[Int, A]) {
+  
+  private val numberOfSides = sides.lastKey
+  
+  private val rnd = new Random
+  
+  def roll(): A = {
+    val d = 1 + rnd.nextInt(numberOfSides)
+    for ((weight, value) <- sides) {
+      if (d <= weight) return value
+    }
+    return sides.last._2
+  }
+}
+
+object Die {
+  
+  case class Side[A](val value: A, val weight: Int)
+  
+  def apply[A](sides: Side[A]*): Die[A] = {
+    var sum = 0
+    val sidesMap = mutable.Map[Int, A]()
+    for (side <- sides) {
+      sum += side.weight
+      sidesMap(sum) = side.value
+    }
+    new Die(TreeMap(sidesMap.toArray:_*))
+  }
+  
+}
---
+++ scala/data-structs/src/test/scala/op2poe/baseball/data/game/DieTest.scala
@@ -0,0 +1,34 @@
+package op2poe.baseball.data.game
+
+import op2poe.baseball.data.game.Die.Side
+import java.util.Arrays
+
+object DieTest extends App {
+
+  testTwoSidedDie()
+  testSixSidedDie()
+  testWeightedSixSidedDie()
+  
+  def testTwoSidedDie() {
+    val die: Die[String] = Die(Side("Even", 1), Side("Odd", 1))
+    println(die.roll())
+    println(die.roll())
+    println(die.roll())
+    println(die.roll())
+  }
+  
+  def testSixSidedDie() {
+    val die = Die((for (v <- 1 to 6) yield Side(v, 1)):_*)
+    val sums = new Array[Int](6)
+    for (_ <- 1 to 1000) sums(die.roll() - 1) += 1
+    println(Arrays.toString(sums))
+  }
+  
+  def testWeightedSixSidedDie() {
+    val die = Die((for (v <- 1 to 6) yield Side(v, v)):_*)
+    val sums = new Array[Int](6)
+    for (_ <- 1 to 1000) sums(die.roll() - 1) += 1
+    println(Arrays.toString(sums))
+  }
+  
+}
