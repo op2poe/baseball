608f741c0039fd1e358f6bf8707c0673fca7170f
Optwo Poe <op2poe@gmail.com>
2012-05-31 05:47:48 -0700

    Added abstract class GameProcessor, which processes individual games in chunks, by reading a play-by-play file.

---
+++ scala/retrosheet/src/main/scala/retrosheet/pbp/GameProcessor.scala
@@ -0,0 +1,43 @@
+package retrosheet.pbp
+
+import scala.io.Source
+
+abstract class GameProcessor {
+
+  private var inning = -1
+  
+  private var battingTeam = -1
+  
+  def processFile(filePath: String) {
+      val source = Source.fromFile(filePath)
+      for (line <- source.getLines) {
+        val parts = line.split(",");
+        parts(0) match {
+          case "id" => newGame()
+          case "data" => endGame()
+          case "play" => inspectPlayRecord(parts)
+        }
+      }
+  }
+  
+  private def inspectPlayRecord(play: Array[String]) {
+    val i = play(1).toInt
+    val t = play(2).toInt
+    if (i != inning || t != battingTeam) {
+      inning = i;
+      battingTeam = t;
+      newInning(i, t)
+    }
+    val event = play(6)
+    processEvent(event)
+  }
+
+  def newGame(): Unit;
+  
+  def newInning(inning: Int, battingTeam: Int): Unit;
+  
+  def processEvent(event: String): Unit;
+  
+  def endGame(): Unit;
+  
+}
