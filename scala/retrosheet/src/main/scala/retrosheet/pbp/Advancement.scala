47224d5c33c0388f8fff32c9dec47646540d2c60
Optwo Poe <op2poe@gmail.com>
2012-06-15 07:52:04 -0700

    Augmented advancement to handle the batter, as well as outs on bases.

--- scala/retrosheet/src/main/scala/retrosheet/pbp/Advancement.scala
+++ scala/retrosheet/src/main/scala/retrosheet/pbp/Advancement.scala
@@ -1,41 +1,71 @@
 package retrosheet.pbp
 
 /**
- * Represents a runner (possibly) advancing from one base. Due to
- * the retrosheet play-by-play format, it is sometimes the case that
- * {@code fromBase} equals {@code toBase} (i.e. no advancement was 
+ * Represents a runner (possibly) advancing from one base.
+ * <p>
+ * The batter is represented by {@code fromBase = 0}. A negative value
+ * of {@code toBase} represents the runner being out at that base.
+ * <p>
+ * Due tothe retrosheet play-by-play format, it is sometimes the case that
+ * {@code fromBase} equals {@code toBase} (i.e. no advancement was
  * actually made).
  * <p>
- * {@code fromBase} and {@code toBase} are 1-based, since the play-by-play 
+ * {@code fromBase} and {@code toBase} are 1-based, since the play-by-play
  * files are.
  */
 case class Advancement(val fromBase: Int, val toBase: Int) {
 
-  require(fromBase > 0)
+  require(fromBase >= 0)
   require(fromBase < 4);
-  require(toBase <= 4)
-  require(toBase >= fromBase)
-  
+  require(toBase.abs > 0)
+  require(toBase.abs <= 4)
+  require(toBase.abs >= fromBase)
+
   /**
-   * Applies this {@code Advancement} object to the current base situation, 
+   * Applies this {@code Advancement} object to the current base situation,
    * by modifying the inputted {@code Bases} object.
    */
   def applyTo(bases: Bases) {
     if (fromBase == toBase) return
+    if (toBase > 0) {
+      // advancement
+      if (fromBase == 0) {
+        advanceBatter(bases)
+      } else {
+        advanceRunner(bases)
+      }
+    } else {
+      // out on base
+      if (fromBase == 0) {
+        // Batter is out, e.g. trying to stretch a single into a double.
+        // Nothing to do.
+      } else {
+        bases(fromBase) = None
+      }
+    }
+  }
+
+  private def advanceBatter(bases: Bases) {
+    if (toBase > 0 & toBase < 4) {
+      bases(toBase) == Some("[batter]")
+    }
+  }
+
+  private def advanceRunner(bases: Bases) {
     val runner = bases(fromBase)
     runner match {
       case None => throw new IllegalBaseOperation("No runner on base " + fromBase)
-      case Some(player) => 
+      case Some(player) =>
         if (toBase < 4) {
-        	bases(toBase) = runner
+          bases(toBase) = runner
         }
         bases remove fromBase
     }
   }
-  
+
   /**
    * Tests if this {@code Advancement} represents a runner reaching home.
    */
   def runScored = (toBase == 4)
-  
+
 }
--- scala/retrosheet/src/main/scala/retrosheet/pbp/Situation.scala
+++ scala/retrosheet/src/main/scala/retrosheet/pbp/Situation.scala
@@ -1,17 +1,41 @@
 package retrosheet.pbp
 
-class Situation private (private val batter:String,
-						 private val bases: Array[Option[String]],
+import scala.util.matching.Regex
+
+class Situation private (private val bases: Bases,
 						 private var outs: Int,
 						 private var runsScored: Int) {
 
+  private var runnerAdvanceRegEx = new Regex("(\\d)-(\\d).*")
+  
+  private var runnerScoredRegEx = new Regex("(\\d)-H.*")
+  
+  private var runnerOutRegEx = new Regex("(\\d)X(\\d|H)")
+  
+  def processEvent(batter: String, event: String) {
+    val parts = event.split("\\.")
+    var batterHasBeenProcessed = parts.length match {
+      case 1 => // TODO
+      case 2 => processAdvancementOfRunners(parts(2))
+    }
+  }
+  
+  private def processAdvancementOfRunners(adv: String): Boolean = {
+    var batter = false
+    val codes = adv.split(";")
+    for (code <- codes) {
+      
+    }
+    return batter
+  }
   
 }
 
+
 object Situation {
   
-  def startOfInning(batter: String) = {
-    new Situation(batter, Array.fill(3)(None), 0, 0)
+  def startOfInning() = {
+    new Situation(Bases.empty(), 0, 0)
   }
   
 }
